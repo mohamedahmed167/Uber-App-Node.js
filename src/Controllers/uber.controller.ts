e79fe0edfe7135887df2c5341b407c92e208d047
mohamedahmed167 <marmo1672017@gmail.com>
2026-08-30 17:30:25 +0300

    add hashed password by use bcrypt and edit Register vaildation

--- src/Controllers/uber.controller.ts
+++ src/Controllers/uber.controller.ts
@@ -1,27 +1,65 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
 import UserModel from "../models/User.model";
-  export const Register = async (req :Request, res:Response) => {
+import bcrypt from "bcrypt";
+export const Register = async (req: Request, res: Response) => {
   try {
     const { name, email, password, role } = req.body;
+    if(!name || typeof name != "string" || !name.trim()){
+      return res.status(400).json({ success:false ,message:'name is required and should be non-empty string'})
+    }
+    if(!email || typeof email != "string" || !email.trim()){
+      return res.status(400).json({success :false  , message:"is required and should be non-empty string"})
+    }
 
-      const existEmail = await UserModel.findOne({ email });
-      if (existEmail) {
-        return res
-          .status(400)
-          .json({ message: "there email is already exist" });
-      }
+    if(!password || typeof password !="string" || password.length <6){
+      return res.status(400).json({success :false ,message:"password is required and should be at least 6 characters long "})
+    }
+    const existEmail = await UserModel.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({
+        success:false ,
+        message: "there email is already exist" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await UserModel.create({
       name: name.trim(),
       email: email.trim().toLowerCase(),
-      password,
+      password: hashedPassword,
       role: role === "driver" ? "driver" : "user",
     });
     await user.save();
-    const userResponse:any =user.toObject();
+    const userResponse: any = user.toObject();
     delete userResponse.password;
-    return res.status(200).json({message:`vaild Register welcome to uber app`,user:userResponse})
+    return res.status(200).json({
+      message: `vaild Register welcome to uber app`,
+      user: userResponse,
+    });
   } catch (error) {
     console.log(error);
-    res.status(400).json({message:"there is problem in Register"})
+    res.status(400).json({ message: "there is problem in Register" });
+  }
+};
+export const Login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).json({ message: "invaild email or password" });
+    }
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "please enter email or password" });
+    }
+    const IsVaild = await bcrypt.compare(password, user.password);
+    if (!IsVaild) {
+      return res.status(400).json({ message: "password is not matched" });
+    }
+    return res.status(200).json({message:`hello ${user.name}`})
+  } catch (error) {
+    console.log("there is error in login", error);
+    res
+      .status(400)
+      .json({ message: "there is error in login please check it" });
   }
 };
